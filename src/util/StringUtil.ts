3acafae41068ad72ba6cdaab785a8697260e3b78
Isaac Yang <isaac.yang@ufinity.com>
2022-01-12 14:53:49 +0800

    feat: updated ndi helper with IdToken interface

--- src/util/StringUtil.ts
+++ src/util/StringUtil.ts
@@ -1,4 +1,5 @@
 import * as isBase64 from "is-base64";
+import { TokenPayload } from "../singpass";
 
 
 export function replaceLineBreaks(input: string): string {
@@ -11,4 +12,29 @@ export function replaceLineBreaks(input: string): string {
 
 export function isBase64Valid(test: string): boolean {
 	return isBase64(test, { paddingRequired: false });
+}
+
+export function extractNricAndUuidFromPayload(payload: TokenPayload): {
+	nric: string;
+	uuid: string;
+} {
+	const { sub } = payload;
+
+	if (sub) {
+		const extractionRegex = /s=([STFG]\d{7}[A-Z]).*,u=(.*)/i;
+		const matchResult = sub.match(extractionRegex);
+
+		if (!matchResult) {
+			throw Error(
+				"Token payload sub property is invalid, does not contain valid NRIC and uuid string"
+			);
+		}
+
+		const nric = matchResult[1];
+		const uuid = matchResult[2];
+
+		return { nric, uuid };
+	}
+
+	throw Error("Token payload sub property is not defined");
 }
